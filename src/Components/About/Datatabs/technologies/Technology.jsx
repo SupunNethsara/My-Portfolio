f98fc6a1b344a4c0076e369ec41f223f681000a5
Supun Nethsara <supunmax663@gmail.com>
2025-07-15 05:14:25 -0700

    fixed issue of technology section

--- src/Components/About/Datatabs/technologies/Technology.jsx
+++ src/Components/About/Datatabs/technologies/Technology.jsx
@@ -1,91 +1,34 @@
 import React from 'react'
 import "./Technology.css"
-import softwareitem1 from '/pngwing1.png';
-import softwareitem2 from '/pngwing2.png';
-import softwareitem3 from '/pngwing3.png';
-import softwareitem4 from '/pngwing4.png';
-import softwareitem5 from '/pngwing5.png';
-import softwareitem6 from '/pngwing6.png';
-import softwareitem7 from '/pngwing7.png';
-import softwareitem8 from '/pngwing8.png';
-import softwareitem9 from '/pngwing9.png';
-import softwareitem10 from '/pngwing10.png';
-import softwareitem11 from '/pngwing11.png';
-import softwareitem12 from '/pngwing12.png';
-
-import softwareitem13 from '/pngwing13.png';
-import softwareitem14 from '/pngwing14.png';
-import softwareitem16 from '/pngwing16.png';
-import softwareitem17 from '/pngwing17.png';
-import softwareitem18 from '/pngwing18.png';
-import softwareitem19 from '/pngwing19.png';
+import { RiReactjsLine } from 'react-icons/ri';
+import { SiFirebase } from 'react-icons/si';
 export const Technology = () => {
   return (
     <div className='tech'>
-      <div className="software">
-        <div className="software_item">
-          <img src={softwareitem1} alt="" />
-        </div>
-        <div className="software_item">
-          <img src={softwareitem2} alt="" />
-        </div>
-        <div className="software_item">
-          <img src={softwareitem3} alt="" />
-        </div>
-        <div className="software_item">
-          <img src={softwareitem4} alt="" />
-        </div>
-        <div className="software_item">
-          <img src={softwareitem5} alt="" />
-        </div>
-        <div className="software_item">
-          <img src={softwareitem6} alt="" />
-        </div>
-
-      </div>
-      <div className="software">
-        <div className="software_item">
-          <img src={softwareitem7} alt="" />
-        </div>
-        <div className="software_item">
-          <img src={softwareitem8} alt="" />
-        </div>
-        <div className="software_item">
-          <img src={softwareitem9} alt="" />
-        </div>
-        <div className="software_item">
-          <img src={softwareitem10} alt="" />
-        </div>
-        <div className="software_item">
-          <img src={softwareitem11} alt="" />
-        </div>
-        <div className="software_item">
-          <img src={softwareitem12} alt="" />
-        </div>
-
-      </div>
-      <div className="software">
-        <div className="software_item">
-          <img src={softwareitem13} alt="" />
-        </div>
-        <div className="software_item">
-          <img src={softwareitem14} alt="" />
-        </div>
-
-        <div className="software_item">
-          <img src={softwareitem16} alt="" />
-        </div>
-        <div className="software_item">
-          <img src={softwareitem17} alt="" />
-        </div>
-        <div className="software_item">
-          <img src={softwareitem18} alt="" />
-        </div>
-        <div className="software_item">
-          <img src={softwareitem19} alt="" />
-        </div>
-
-      </div>
+    
+                <div className="mt-3">
+               
+                  import {RiReactjsLine} from 'react-icons/ri';
+                  import {SiFirebase} from 'react-icons/si';
+    
+                  <div className="flex flex-wrap justify-center sm:justify-start gap-3">
+                    {[
+                      'React', 'laravel', 'JavaScript', 'TypeScript', 'Cloudinary', 'next.js',
+                      'Node.js', 'Firebase', 'Tailwind CSS', 'mySQL', 'Redux',
+                    ].map((skill) => (
+                      <span
+                        key={skill}
+                        className="px-4 py-2 rounded-full text-sm font-medium bg-gray-800/50 border border-gray-700 text-gray-200 hover:bg-gray-700/50 hover:text-white hover:border-red-400/50 transition-all flex items-center"
+                      >
+                        {skill}
+    
+                        {skill === 'React' && <RiReactjsLine className="ml-1.5 text-blue-400" />}
+                        {skill === 'Firebase' && <SiFirebase className="ml-1.5 text-orange-400" />}
+                      </span>
+                    ))}
+                  </div>
+    
+                </div>
     </div>
   )
 }
